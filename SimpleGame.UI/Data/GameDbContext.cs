af592af41aad55561a9374eab348797567023cd5
Behnam M <bmm.ice@gmail.com>
2025-05-13 17:10:08 +0300

    Add GameDbContext and EF Core migrations for Game and Card tables

---
+++ SimpleGame.UI/Data/GameDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+using SimpleGame.UI.Models;
+using System.Collections.Generic;
+
+namespace SimpleGame.UI.Data
+{
+	public class GameDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public GameDbContext(DbContextOptions<GameDbContext> options) : base(options) { }
+
+		public DbSet<Card> Cards { get; set; }
+		public DbSet<Game> Games { get; set; }
+		public DbSet<Player> Players { get; set; }
+	}
+}
---
+++ SimpleGame.UI/Migrations/20250513111425_GoFish_Sync_01.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleGame.UI.Migrations
+{
+    /// <inheritdoc />
+    public partial class GoFish_Sync_01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrentTurn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsGameOver = table.Column<bool>(type: "bit", nullable: false),
+                    Winner = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GamesPlayed = table.Column<int>(type: "int", nullable: false),
+                    GamesWon = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Rank = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Owner = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GameId = table.Column<int>(type: "int", nullable: true),
+                    PlayerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cards_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Cards_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_GameId",
+                table: "Cards",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_PlayerId",
+                table: "Cards",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
